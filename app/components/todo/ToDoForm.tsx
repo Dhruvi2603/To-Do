d3dcec424f1b5b3615fc2bedb0ab5fb609e3bdea
Dhruvi Patel <pateldhruvi2611@gmail.com>
2026-06-28 21:51:25 +0530

    Add to do functionality

---
+++ app/components/todo/ToDoForm.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Box, Button, Container, Grid, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  text: yup
+    .string()
+    .required("To-Do is required")
+    .min(3, "Minimum 3 characters is required"),
+});
+
+interface Props {
+  onAddToDo: (text: string) => void
+}
+
+interface FormValues {
+  text: string;
+}
+
+const ToDoForm = ({onAddToDo}: Props) => {
+  const { control, handleSubmit, reset } = useForm<FormValues>({
+    defaultValues: {
+      text: "",
+    },
+    resolver: yupResolver(validationSchema),
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: FormValues) => {
+    onAddToDo(data.text);
+    reset();
+  }
+
+  return (
+    <>
+      <Container maxWidth="xl" sx={{ mt: "50px" }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={3}>
+          <Grid size={{ xs: 10 }}>
+            <Controller
+              control={control}
+              name="text"
+              render={({ field, fieldState }) => (
+                <TextField
+                  {...field}
+                  variant="outlined"
+                  fullWidth
+                  placeholder="Enter your to-do"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                />
+              )}
+            />
+          </Grid>
+          <Grid size={{ xs: 2 }}>
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              sx={{ color: "#fff", textTransform: "none", height: "52px" }}
+            >
+              Add To-Do
+            </Button>
+          </Grid>
+        </Grid>
+        </form>
+      </Container>
+    </>
+  );
+};
+
+export default ToDoForm;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
+"use client";
+
 import { Box } from "@mui/material";
+import ToDoForm from "./components/todo/ToDoForm";
+import { useEffect, useState } from "react";
+import { ToDo } from "./components/types/todo";
 
 export default function Home() {
+  const [todos, setToDos] = useState<ToDo[]>([]);
+
+  useEffect(() => {
+    const storedToDos = localStorage.getItem("todos");
+
+    if (storedToDos) {
+      setToDos(JSON.parse(storedToDos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addToDo = (text: string) => {
+    const newToDo: ToDo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+    setToDos([...todos, newToDo]);
+  };
+
   return (
     <Box>
-      Home page
+      <ToDoForm onAddToDo={addToDo} />
     </Box>
   );
 }
